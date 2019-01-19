216b03775a91e1e7a68e80c9c4b2cdc6d11d247b
Andrew Dinsmore <teif26@outlook.com>
2019-01-18 22:58:11 -0500

    most of the js finished

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const gridContainer = document.querySelector("#container_inner");
-var size = 16;
+var size = 36;
 
 const initializeGrid = (gridContainer,size) =>{
-  for(let i = 0;i<size;i++){
+  for(let i = 0;i<=size;i++){
       setGridSize(gridContainer,size)
-      for(let j = 0;j<size;j++){
+      for(let j = 0;j<=size;j++){
           const square = document.createElement("div");
           square.classList.add("square");
           gridContainer.appendChild(square);
@@ -15,4 +15,9 @@ const setGridSize = (gridContainer, size)=>{
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 };
+const addListener = (square)=>{
+    
+}
+
+
 initializeGrid(gridContainer,size)
