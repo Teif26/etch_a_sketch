09c3362e238e3305eff11bb379cca6dd27a9c177
Andrew Dinsmore <teif26@outlook.com>
2019-01-21 00:08:52 -0500

    Shake button working to shake nd clear

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const shakeBtn = document.getElementById("shake")
+const rdmColor = document.getElementById("rdmColor")
 const gridContainer = document.querySelector("#container_inner");
-var size = 216;
+var size = 136;
 
 const initializeGrid = (gridContainer,size) =>{
   for(let i = 0;i<=size;i++){
@@ -9,7 +11,7 @@ const initializeGrid = (gridContainer,size) =>{
           square.classList.add("square");
           gridContainer.appendChild(square);
          
-         square.addEventListener("mouseenter", (el) =>{
+         square.addEventListener("mouseover", (el) =>{
          const opacity = getComputedStyle(el.target).getPropertyValue("--opacity");
             if(opacity <= 1){
                 return el.target.style.setProperty("--opacity", parseFloat(opacity,10) + .1);
@@ -24,7 +26,24 @@ const setGridSize = (gridContainer, size)=>{
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 };
 
+shakeBtn.addEventListener("click", function(){
+    var main = document.getElementById("container_main")
+ main.style.animation = "shake .5s 1";
+ var newScreen = document.querySelectorAll(".square");
+   newScreen.forEach((div)=>{
+    return div.style.setProperty("opacity", 0)   
+   })
+ 
 
+})
+
+// function shakeInitialize(){
+//  var main = document.getElementById("container_main")
+//  main.style.animation = "shake .5s 1"
+//  const newScreen = document.querySelectorAll(".square")
+ 
+//  return newScreen.style.setProperty("opacity, 0")
+// }
 
 
 
